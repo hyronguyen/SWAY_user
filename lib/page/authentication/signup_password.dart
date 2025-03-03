aa1643be3a2394303e0c4c1ba4134d58f7b53d70
hoangnghiahp0107 <hoangnghiahp0107@gmail.com>
2025-03-03 16:04:49 +0700

    THEM SIGNIN SIGNUP RESENDOTP VERIFYOTP

--- lib/page/authentication/signup_password.dart
+++ lib/page/authentication/signup_password.dart
@@ -1,23 +1,23 @@
 import 'package:flutter/material.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sway/Controller/user_controller.dart';
+import 'package:sway/data/model/user.dart';
 import 'package:sway/page/authentication/login.dart';
-import 'package:encrypt/encrypt.dart' as encrypt;
+import 'package:sway/page/authentication/signup_email_verify.dart'; // Đảm bảo có màn hình LoginScreen
 
 class SetPasswordScreen extends StatefulWidget {
-  ////////////////////////////// Biến Widgets ///////////////////////////////////////////////////////////////
-  final String userId;
-  final String name;
-  final String mail;
+  final String fullname;
+  final String email;
   final String? gender;
-  final String phoneNumber;
+  final String phone;
+  final String birthday;
 
   const SetPasswordScreen({
     super.key,
-    required this.phoneNumber,
-    required this.name,
-    required this.mail,
+    required this.phone,
+    required this.fullname,
+    required this.email,
     required this.gender,
-    required this.userId,
+    required this.birthday,
   });
 
   @override
@@ -25,23 +25,97 @@ class SetPasswordScreen extends StatefulWidget {
 }
 
 class _SetPasswordScreenState extends State<SetPasswordScreen> {
-  ////////////////////////////// Biến cục bộ ///////////////////////////////////////////////////////////////
   TextEditingController passwordController = TextEditingController();
   TextEditingController confirmPasswordController = TextEditingController();
   bool isLoading = false;
   bool obscureText = true;
-  static const String secretKey = "12345678901234567890123456789012";
-  static const String ivKey = "1234567890123456";
+  final UserController userController = UserController(); // Khởi tạo Usercontroller
 
+  // Hàm đăng ký người dùng
+  Future<void> SignUp() async {
+    String password = passwordController.text.trim();
+    String confirmPassword = confirmPasswordController.text.trim();
 
-////////////////////////////// LAYOUT ///////////////////////////////////////////////////////////////
+    // Kiểm tra nếu mật khẩu và xác nhận mật khẩu không trùng khớp
+    if (password != confirmPassword) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Mật khẩu và xác nhận mật khẩu không khớp")),
+      );
+      return;
+    }
+
+    // Kiểm tra độ dài mật khẩu (ví dụ: phải ít nhất 8 ký tự)
+    if (password.length < 8) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Mật khẩu phải có ít nhất 8 ký tự")),
+      );
+      return;
+    }
+
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      // Dữ liệu người dùng để gửi API (không mã hóa mật khẩu vì bạn đã mã hóa ở backend)
+      User userData = User(
+        fullname: widget.fullname,
+        email: widget.email,
+        phone: widget.phone,
+        gender: widget.gender,
+        birthday: widget.birthday,
+        password: password, // Sử dụng mật khẩu trực tiếp (không mã hóa)
+      );
+
+      // Gọi API signUp (Giả sử bạn có một hàm `signUp` trong UserController để gọi API backend)
+      bool signupSuccess = await userController.signUp(userData);
+
+      if (signupSuccess) {
+        // Nếu đăng ký thành công, điều hướng người dùng đến màn hình đăng nhập
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Đăng ký thành công!")),
+        );
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => SignupEmailVerifyScreen(email: widget.email)), // Chuyển hướng về trang đăng nhập
+        );
+      } else {
+        // Nếu có lỗi khi đăng ký
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Đăng ký thất bại. Vui lòng thử lại.")),
+        );
+      }
+    } catch (e) {
+      print("Error: $e");
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Có lỗi xảy ra. Vui lòng thử lại.")),
+      );
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  // Hàm kiểm tra email
+  bool isValidEmail(String email) {
+    final RegExp regex = RegExp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$');
+    return regex.hasMatch(email);
+  }
+
+  // Hàm kiểm tra số điện thoại
+  bool isValidPhone(String phone) {
+    final RegExp regex = RegExp(r'^\d{10,11}$'); // Kiểm tra số điện thoại có 10-11 chữ số
+    return regex.hasMatch(phone);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Thiết lập mật khẩu"),
         centerTitle: true,
-        automaticallyImplyLeading: false
+        automaticallyImplyLeading: false,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
@@ -56,16 +130,15 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
             const SizedBox(height: 10),
 
             // Ô nhập mật khẩu mới
-              TextField(
+            TextField(
               controller: passwordController,
               obscureText: obscureText,
               decoration: InputDecoration(
                 labelText: "Mật khẩu mới",
-                labelStyle: TextStyle(color: Colors.white),
-                border: OutlineInputBorder(), // Viền mặc định
-                focusedBorder: OutlineInputBorder(
+                labelStyle: const TextStyle(color: Colors.white),
+                border: const OutlineInputBorder(), // Viền mặc định
+                focusedBorder: const OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.white, width: 2.0), // Viền xanh khi focus
-                  borderRadius: BorderRadius.circular(8.0),
                 ),
                 suffixIcon: IconButton(
                   icon: Icon(
@@ -86,24 +159,22 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
             TextField(
               controller: confirmPasswordController,
               obscureText: true,
-              decoration: InputDecoration(
+              decoration: const InputDecoration(
                 labelText: "Xác nhận mật khẩu",
                 labelStyle: TextStyle(color: Colors.white),
                 border: OutlineInputBorder(), // Viền mặc định
                 focusedBorder: OutlineInputBorder(
                   borderSide: BorderSide(color: Colors.white, width: 2.0), // Viền xanh khi focus
-                  borderRadius: BorderRadius.circular(8.0),
                 ),
               ),
             ),
-
 
             const SizedBox(height: 15),
 
             // Gợi ý tạo mật khẩu mạnh
             const Text(
               "⚡ Gợi ý tạo mật khẩu mạnh:",
-              style: TextStyle(fontWeight: FontWeight.bold, color:  Color(0xFFedae10)),
+              style: TextStyle(fontWeight: FontWeight.bold, color: Color(0xFFedae10)),
             ),
             const SizedBox(height: 5),
             const Text(
@@ -118,77 +189,21 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
               child: isLoading
                   ? const CircularProgressIndicator()
                   : ElevatedButton(
-                      onPressed: savePassword,
+                      onPressed: SignUp,
                       style: ElevatedButton.styleFrom(
-                          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
-                          backgroundColor: Color(0xFFedae10), // Đổi màu nền nút
-                          foregroundColor: Colors.white, // Đổi màu chữ trên nút
-                          shape: RoundedRectangleBorder(
+                        padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 12),
+                        backgroundColor: const Color(0xFFedae10), // Đổi màu nền nút
+                        foregroundColor: Colors.white, // Đổi màu chữ trên nút
+                        shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(12), // Bo tròn viền nút
-                          ),
+                        ),
                       ),
                       child: const Text("Xác nhận", style: TextStyle(fontSize: 16)),
                     ),
             ),
-            
           ],
         ),
       ),
     );
-  }
-
-////////////////////////////// Function ///////////////////////////////////////////////////////////////
-  // Mã hoá mật khẩu
-  String encryptPassword(String password) {
-    final key = encrypt.Key.fromUtf8(secretKey);
-    final iv = encrypt.IV.fromUtf8(ivKey);
-    final encrypter = encrypt.Encrypter(encrypt.AES(key, mode: encrypt.AESMode.cbc));
-    final encrypted = encrypter.encrypt(password, iv: iv);
-    return encrypted.base64;
-  }
-
-  // Lưu mật khẩu
-  Future<void> savePassword() async {
-    String password = passwordController.text.trim();
-    String confirmPassword = confirmPasswordController.text.trim();
-
-    if (password.length < 8) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Mật khẩu phải có ít nhất 8 ký tự")),
-      );
-      return;
-    }
-    if (password != confirmPassword) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Mật khẩu xác nhận không khớp")),
-      );
-      return;
-    }
-
-    setState(() => isLoading = true);
-
-    try {
-      String encryptedPassword = encryptPassword(password);
-      await FirebaseFirestore.instance.collection("USERS").doc(widget.userId).set({
-        "user_password": encryptedPassword,
-        "user_name": widget.name,
-        "user_phone": widget.phoneNumber,
-        "user_gender": widget.gender,
-        "user_mail": widget.mail,
-      });
-
-      if (!mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text("Mật khẩu đã được thiết lập thành công!")),
-      );
-
-      Navigator.push(context, MaterialPageRoute(builder: (c) => LoginScreen()));
-    } catch (e) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Lỗi: ${e.toString()}")),
-      );
-    } finally {
-      setState(() => isLoading = false);
-    }
   }
 }
